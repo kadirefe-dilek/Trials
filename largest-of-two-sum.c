05973b9010aac1d9f75c940e2be25b76cd48c795
Kadir Efe Dilek <71722234+kadirefe-dilek@users.noreply.github.com>
2021-03-10 18:30:30 +0300

    Create largest-of-two-sum.c

---
+++ largest-of-two-sum.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+  //declaring variables to be used.
+	int how_many, number, max1, max2; //max1 to be the largest and max2 the second largest 
+	int sum = 0;  //sum is initialized to 0 as default
+	
+	printf("How many numbers will you enter?\t");
+	scanf("%d", &how_many);
+	puts("");
+  
+	while(how_many != 0) {
+	    printf("Enter an integer:\t");
+	    scanf("%d", &number);
+	    
+        if(number > max1) {
+            max2= max1; //max1 is now second greatest
+            max1 = number;  //assigning new max1
+        }
+        else if (number > max2) {
+            max2 = number;  //assigning new max2 value
+        }
+        
+      puts("");
+	    how_many--;
+	}
+	printf("Two largest numbers are: %d and %d.\n", max1, max2);
+	sum = max1 + max2;
+	printf("Sum of two largest is: %d", sum);
+
+	return 0;
+}
